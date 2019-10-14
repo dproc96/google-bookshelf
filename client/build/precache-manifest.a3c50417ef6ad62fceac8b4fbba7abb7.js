6d33832bef5e959ef33b70a55afffcb0764338b7
Dan Proctor <dproc96@gmail.com>
2019-10-14 13:40:38 -0400

    unignored build

---
+++ client/build/precache-manifest.a3c50417ef6ad62fceac8b4fbba7abb7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bb3db9b8e778888f36fed16d714dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ea7302446b7594857c1",
+    "url": "/static/js/2.4d1e4bfb.chunk.js"
+  },
+  {
+    "revision": "0893722e41f2d8190248",
+    "url": "/static/js/main.249ba554.chunk.js"
+  },
+  {
+    "revision": "b5ecc6ebeb7d1e88e4dd",
+    "url": "/static/js/runtime-main.f6bd4088.js"
+  }
+]);
